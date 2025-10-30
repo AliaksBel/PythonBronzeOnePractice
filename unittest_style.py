cf880c0789beb4b695467c2611699d4018d928d4
Aleksandr Belazor <avbelazor@gmail.com>
2025-10-30 13:09:09 +0300

    обавлены последние изменения по тестам

--- unittest_style.py
+++ unittest_style.py
@@ -11,37 +11,33 @@ class TestRegistration(unittest.TestCase):
         self.browser.quit()
     
     def test_registration1(self):
-        # Первая страница регистрации
         self.browser.get("http://suninjuly.github.io/registration1.html")
         
-        # Заполняем обязательные поля
+        # Заполнить поля
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Ivan")
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Petrov")
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("test@mail.com")
         
-        # Отправляем заполненную форму
+        # Отправить
         button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         
-        # Проверяем успешность регистрации
+        # Проверить регистрацию
         time.sleep(5)
         welcome_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
     
     def test_registration2(self):
-        # Вторая страница регистрации (с багом)
         self.browser.get("http://suninjuly.github.io/registration2.html")
         
-        # Заполняем обязательные поля
+        # Заполнить поля
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .first").send_keys("Ivan")
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .second").send_keys("Petrov")
         self.browser.find_element(By.CSS_SELECTOR, ".first_block .third").send_keys("test@mail.com")
         
-        # Отправляем заполненную форму
         button = self.browser.find_element(By.CSS_SELECTOR, "button.btn")
         button.click()
         
-        # Проверяем успешность регистрации
         time.sleep(5)
         welcome_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertEqual("Congratulations! You have successfully registered!", welcome_text)
